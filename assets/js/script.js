2643dd916a195b5efd3c49204461ed0a5c273c0b
Manny <vmlujanjr@outlook.com>
2022-03-25 11:32:35 -0500

    added sortable & drag-n-drop functionality to items using jQueryUI and Touch Punch

--- assets/js/script.js
+++ assets/js/script.js
@@ -170,6 +170,74 @@ $(".list-group").on("blur", "input", function() {
 
 });
 
+// creates sortable list; and connects it with others of their same class
+$(".card .list-group").sortable({ // connect IDs or Classes INDIVIDUALLY
+  connectWith: $(".card .list-group"), // connect IDs or Classes (OR GROUP) TOGETHER
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone", // need to ask about this in more detail
+  activate: function(event) { // need to ask about this in more detail
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    // array to store the task data in // IMPORTANT: Make sure you declare it first, otherwise it'll be headache to debug and test while you're working here
+    var tempArr = [];
+    
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+      console.log(text, date);
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+
+    // trim down list's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+    
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function (event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
 
 
 
